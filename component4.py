ebd5d2ffdab477ce5852f0aafeecf7a7dd043f80
Nicole Binder <>
2021-04-15 21:11:04 +0000

    updated component 4

--- component4.py
+++ component4.py
@@ -1,6 +1,7 @@
 from MarcovModel import MarcovModel
 
 def component4():
+  # Basic idea for the hybrid generation is to prioritize meaning over syntax. In other words, we generate a text using the original generate method. Then you correct any jarring grammar mistakes with the pos generation scheme.
   print("-----working on component4 (Check 'results' folder)-----")
   f = open("results/component4.txt", "a")
   model = MarcovModel("arthur_conan_doyle_collected_works.txt", level = "word", order = 3, hybrid = True)
